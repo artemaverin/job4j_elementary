c09fef05bb56102d384b243338747b6b20030ae4
Artem Averin <artem-averin-91@mail.ru>
2022-05-15 22:26:00 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Массив ages типа short содержит: " + ages.length + " элементов");
+        System.out.println("Массив surnames типа String содержит: " + surnames.length + " элементов");
+        System.out.println("Массив prices типа float содержит: " + prices.length + " элементов");
     }
 }
