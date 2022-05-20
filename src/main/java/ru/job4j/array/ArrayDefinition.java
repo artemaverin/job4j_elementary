ad891fc402248027f21d53f8e58ba363ddaaf51c
Artem Averin <artem-averin-91@mail.ru>
2022-05-20 23:23:49 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,17 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Массив ages типа short содержит: " + ages.length + " элементов");
         System.out.println("Массив surnames типа String содержит: " + surnames.length + " элементов");
         System.out.println("Массив prices типа float содержит: " + prices.length + " элементов");
-
         String[] names = new String[4];
         names[0] = "Harry Kane";
         names[1] = "Phil Foden";
         names[2] = "Jack Grealish";
         names[3] = "Ollie Watkins";
-
         System.out.println();
         System.out.println(names[0]);
         System.out.println(names[1]);
