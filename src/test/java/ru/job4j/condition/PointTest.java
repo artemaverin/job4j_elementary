d2a12f55dfa907b27c4a64ae20b5bd6b3e341e57
Artem Averin <artem-averin-91@mail.ru>
2022-05-21 23:07:36 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PointTest {
@@ -8,45 +9,37 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
-
     }
 
     @Test
     public void whenMinus35to01then5() {
         double expected = 5;
-        int x1 = -3;
-        int y1 = 5;
-        int x2 = 0;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(-3, 5);
+        Point b = new Point(0, 1);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when213to50then13dot341664064126334() {
         double expected = 13.34;
-        int x1 = 2;
-        int y1 = 13;
-        int x2 = 5;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(2, 13);
+        Point b = new Point(5, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when10to50then4() {
         double expected = 4;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(1, 0);
+        Point b = new Point(5, 0);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
+
 }
