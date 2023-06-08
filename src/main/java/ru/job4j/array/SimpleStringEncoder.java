ec14828ae16ab43fb8be2381867cfdd744f40d69
Artem Averin <artem-averin-91@mail.ru>
2023-06-08 20:35:09 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049 # [#505049]]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else {
+                result += symbol;
+                if (counter > 1) {
+                    result += counter;
+                }
+                counter = 1;
+                symbol = input.charAt(i);
+
+            }
+        }
+        return counter > 1 ? result + symbol + counter : result + symbol;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SimpleStringEncoder.encode("aaaaaaaaaaaabbbcdddd"));
+    }
+}
